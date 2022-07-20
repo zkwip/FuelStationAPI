a1ec4cc13f7e9ccd3271109ec179e8e8355470f9
Joep Bernards <joep@duali.xyz>
2022-07-20 17:39:46 +0200

    bit of cleanuo

--- FuelStationAPI/Controllers/GibGasController.cs
+++ FuelStationAPI/Controllers/GibGasController.cs
@@ -24,17 +24,23 @@ namespace FuelStationAPI.Controllers
             _stationListSources = stationListSources;
         }
 
+        private readonly Predicate<FuelStationIdentifier> AllStations = s => true;
+
         [HttpGet("GetStations")]
-        public async Task<List<FuelStationIdentifier>> GetStationsAsync() 
+        public async Task<List<FuelStationIdentifier>> GetStationsAsync(Predicate<FuelStationIdentifier>? which)
         {
             var results = await Task.WhenAll(_stationListSources.Select(x => x.GetStationListAsync()));
             var list = new List<FuelStationIdentifier>();
+
+
+            if (which is null)
+                which = AllStations;
 
             foreach (var item in results)
             {
                 if (item.Succes)
                 {
-                    list.AddRange(item.Result);
+                    list.AddRange(item.Result.FindAll(which));
                 }
             }
 
@@ -42,13 +48,55 @@ namespace FuelStationAPI.Controllers
         }
 
         [HttpGet("GetPrices")]
-        public async Task<MappedScanResult<List<FuelPriceResult>>> GetPricesAsync(FuelStationIdentifier station)
-        {
-            IFuelPriceDataSource? priceSource = GetPriceSource(station);
-            if (priceSource is null) 
-                return MappedScanResult<List<FuelPriceResult>>.Fail("no data provider");
 
-            return await priceSource.GetPricesAsync(station);
+        public async Task<List<FuelPriceResult>> GetPricesAsync(FuelStationIdentifier station) => await GetPricesAsync(station, null);
+        private async Task<List<FuelPriceResult>> GetPricesAsync(FuelStationIdentifier station, Predicate<FuelType>? fuels)
+        { 
+            IFuelPriceDataSource? priceSource = GetPriceSource(station);
+            if (priceSource is null)
+                throw new Exception();
+
+            if (fuels is null)
+                fuels = FuelTypeExtensions.Gasoline;
+
+            var prices = await priceSource.GetPricesAsync(station);
+            
+            if (!prices.Succes)
+                _logger.LogWarning(prices.Message);
+            
+            var list = prices.Result;
+
+            if(list.Count == 0)
+                _logger.LogWarning("no prices found for station {0}", station);
+
+
+            return list.FindAll(x => fuels(x.FuelType));
+        }
+
+        [HttpGet("ListFilteredStations")]
+        public async Task<IEnumerable<FuelStationIdentifier>> ListFilteredStationsAsync(Predicate<FuelStationIdentifier>? which = null)
+        {
+            IEnumerable<FuelStationIdentifier> list = await GetStationsAsync(which);
+            list = list.Where(x => Geolocation.Distance(x.Location, _location) < 15);
+            return list;
+        }
+
+        private async Task<FuelStationPricesPair> GetStationPricePairAsync(FuelStationIdentifier station, Predicate<FuelType>? fuels = null)
+        {
+            var prices = await GetPricesAsync(station, fuels);
+            return new FuelStationPricesPair(station, prices);
+        }
+
+        [HttpGet("GetAllPrices")]
+        public async Task<List<FuelStationPricesPair>> GetAllPricesAsync() => await GetAllPricesAsync(null, null);
+
+
+        private async Task<List<FuelStationPricesPair>> GetAllPricesAsync(Predicate<FuelStationIdentifier>? which = null, Predicate<FuelType>? fuels = null)
+        {
+            var stations = await GetStationsAsync(which);
+
+            var pairs = await Task.WhenAll(stations.Select(x => GetStationPricePairAsync(x, fuels)));
+            return pairs.Where(x => x.Prices.Any()).ToList();
         }
 
         private IFuelPriceDataSource? GetPriceSource(FuelStationIdentifier station)
---
+++ FuelStationAPI/FuelStationPricesPair.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FuelStationAPI
+{
+    public class FuelStationPricesPair
+    {
+        public FuelStationIdentifier Station { get; }
+
+        public IEnumerable<FuelPriceResult> Prices { get; }
+
+        public FuelStationPricesPair(FuelStationIdentifier station, IEnumerable<FuelPriceResult> prices)
+        {
+            Prices = prices;
+            Station = station;
+        }
+    }
+}
